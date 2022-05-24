10ef3a7e940c902f982a22c289a521c1e46ab109
kreuzerk <kevin.kreuzer90@icloud.com>
2022-05-24 15:41:31 +0200

    feat: 🎸 (context) add context util

---
+++ packages/nx/src/generators/utils/context.util.spec.ts
@@ -0,0 +1,32 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree } from '@nrwl/devkit';
+import * as inquirer from 'inquirer';
+
+import {
+  createConfigFileIfNonExisting,
+  CONFIG_FILE_NAME,
+} from './context.util';
+
+describe('foo generator', () => {
+  let tree: Tree;
+
+  beforeEach(() => {
+    inquirer.prompt = jest.fn();
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should create a new AX file with the scopes if no file exists', async () => {
+    const expectedContexts = ['foo', 'bar', 'baz'];
+    inquirer.prompt.mockReturnValueOnce(
+      Promise.resolve({
+        availableContexts: expectedContexts.join(','),
+      })
+    );
+
+    await createConfigFileIfNonExisting(tree);
+
+    const configFile = tree.read(CONFIG_FILE_NAME);
+    const contextsFromFile = JSON.parse(configFile.toString()).contexts;
+    expect(expectedContexts).toEqual(contextsFromFile);
+  });
+});
---
+++ packages/nx/src/generators/utils/context.util.ts
@@ -0,0 +1,39 @@
+import { Tree } from '@nrwl/devkit';
+import * as inquirer from 'inquirer';
+
+export const CONFIG_FILE_NAME = '.ax.config.json';
+
+interface ConfigFile {
+  contexts?: string[];
+}
+
+export async function createConfigFileIfNonExisting(tree: Tree) {
+  const configurationFileBuffer = tree.read(CONFIG_FILE_NAME);
+  const configFile: ConfigFile = configurationFileBuffer
+    ? JSON.parse(configurationFileBuffer.toString())
+    : null;
+
+  if (!configFile) {
+    console.log(
+      `No configuraiton file found. Don't worry, we will create one for you.`
+    );
+  }
+
+  if (!configFile || !configFile.contexts) {
+    const contexts = await inquirer.prompt({
+      name: 'availableContexts',
+      message:
+        'Please enter all contexts (comma separated) you want to use in your project.',
+    });
+    console.log('Contexts', contexts);
+    tree.write(
+      CONFIG_FILE_NAME,
+      JSON.stringify({ contexts: contexts.availableContexts.split(',') })
+    );
+  }
+}
+
+export function getConfigurations(tree: Tree) {
+  const configurationFileBuffer = tree.read(CONFIG_FILE_NAME);
+  return JSON.parse(configurationFileBuffer.toString());
+}
