e32026492310d95664dbc11ce415431173948f07
kreuzerk <kevin.kreuzer90@icloud.com>
2022-07-22 18:29:15 +0200

    refactor: 💡 (generators) refactor app generator

--- packages/nx/src/generators/app/generator.ts
+++ packages/nx/src/generators/app/generator.ts
@@ -1,17 +1,21 @@
 import * as inquirer from 'inquirer';
-import {organizeImports} from 'import-conductor';
-import {formatFiles, installPackagesTask, Tree, updateJson,} from '@nrwl/devkit';
-import {applicationGenerator} from '@nrwl/angular/generators';
+import { organizeImports } from 'import-conductor';
+import { moveGenerator } from '@nrwl/workspace/generators';
+import { applicationGenerator } from '@nrwl/angular/generators';
+import {
+  formatFiles,
+  installPackagesTask,
+  Tree,
+  updateJson,
+} from '@nrwl/devkit';
 
-// TODO do we want to go with stylelint?
-/*
-import { configurationGenerator, scssGenerator } from 'nx-stylelint';
-*/
 import moduleBoundariesUpdate from '../module-boundaries-update/generator';
+import {
+  createConfigFileIfNonExisting,
+  getContexts,
+} from '../utils/context.util';
 
-import {AppGeneratorOptions} from './schema';
-import {createConfigFileIfNonExisting, getContexts} from '../utils/context.util';
-import {moveGenerator} from "@nrwl/workspace/generators";
+import { AppGeneratorOptions } from './schema';
 
 export default async function generateWorkspaceApp(
   tree: Tree,
@@ -22,82 +26,34 @@ export default async function generateWorkspaceApp(
   validateSchema(schema);
 
   const { context, name } = schema;
-  // TODO should this be configurable?
-  const prefix = `somePrefix-${context}`;
-  const path = `${context}/${name}`;
-  const enrichedSchema = {
-    name: path,
-    style: 'scss' as any,
+
+  await applicationGenerator(tree, {
+    name: `${context}/${name}`,
+    style: 'scss',
     routing: true,
-    buildable: true,
     tags: `context:${context},type:app`,
     standaloneConfig: true,
-    prefix,
-  };
-  const projectName = `${context}-${name}`;
-
-  await applicationGenerator(tree, enrichedSchema);
+    prefix: `somePrefix-${context}`,
+  });
   await moveGenerator(tree, {
     destination: `${context}/${name}-e2e`,
     projectName: `${context}-${name}-e2e`,
     updateImportPath: true,
   });
+
+  await updateProjectTags(tree, context, name);
+  await updatePackageJSONScripts(tree, context, name);
+  await moduleBoundariesUpdate(tree, { context, scope: `${name}` });
+
   removeInitialNavigationConfig(tree, context, name);
   removeWelcomeComponent(tree, context, name);
 
-  await updateJson(
-    tree,
-    `apps/${context}/${name}-e2e/project.json`,
-    (projectJson) => {
-      projectJson.tags = [`context:${context}`, 'type:e2e'];
-      return projectJson;
-    }
-  );
-
-  await updateJson(tree, `package.json`, (packageJson) => {
-    packageJson.scripts[
-      `serve:${projectName}:app`
-    ] = `nx serve --project ${projectName} -o`;
-    packageJson.scripts[
-      `build:${projectName}`
-    ] = `nx build --project ${projectName}`;
-    packageJson.scripts[
-      `analyze:${projectName}`
-    ] = `nx build --project ${projectName} --stats-json && webpack-bundle-analyzer dist/apps/${context}/${name}/stats.json`;
-    packageJson.scripts[
-      `lint:${projectName}`
-    ] = `nx lint --project ${projectName} && nx stylelint --project ${projectName} --fix`;
-    packageJson.scripts[
-      `test:${projectName}`
-    ] = `nx test --project ${projectName}`;
-    packageJson.scripts[
-      `e2e:${projectName}`
-    ] = `nx e2e --project ${projectName}-e2e`;
-    return packageJson;
-  });
-
-  // TODO do we want to use stylelint
-  // await scssGenerator(tree, { project: projectName, skipFormat: true });
-  await moduleBoundariesUpdate(tree, { context, scope: `${name}` });
   await formatFiles(tree);
   await organizeImportStatements(tree, context, name);
 
   return () => {
     installPackagesTask(tree);
   };
-}
-
-async function organizeImportStatements(
-  tree: Tree,
-  context: string,
-  name: string
-) {
-  const modulePath = `apps/${context}/${name}/src/app/app.module.ts`;
-  const appModuleContent = tree.read(modulePath).toString();
-  const appModuleContentWithOrganizedImports = await organizeImports(
-    appModuleContent
-  );
-  tree.write(modulePath, appModuleContentWithOrganizedImports);
 }
 
 async function promptMissingSchemaProperties(
@@ -122,11 +78,23 @@ async function promptMissingSchemaProperties(
         {
           type: 'input',
           name: 'name',
-          message:
-            'What is the application name?',
+          message: 'What is the application name?',
         },
       ])
     ).name;
+  }
+}
+
+function validateSchema(schema: AppGeneratorOptions) {
+  const { name } = schema;
+  if (name.includes(' ')) {
+    throw new Error(
+      `The app name "${name}" should not contain spaces. Please use "-" instead.`
+    );
+  }
+
+  if (name.endsWith('-')) {
+    throw new Error(`The app name "${name}" should not end with "-"`);
   }
 }
 
@@ -174,15 +142,55 @@ function removeWelcomeComponent(tree: Tree, context: string, name: string) {
   tree.write(specPath, specContent);
 }
 
-function validateSchema(schema: AppGeneratorOptions) {
-  const { name } = schema;
-  if (name.includes(' ')) {
-    throw new Error(
-      `The app name "${name}" should not contain spaces. Please use "-" instead.`
-    );
-  }
+async function updateProjectTags(tree: Tree, context: string, name: string) {
+  await updateJson(
+    tree,
+    `apps/${context}/${name}-e2e/project.json`,
+    (projectJson) => {
+      projectJson.tags = [`context:${context}`, 'type:e2e'];
+      return projectJson;
+    }
+  );
+}
 
-  if (name.endsWith('-')) {
-    throw new Error(`The app name "${name}" should not end with "-"`);
-  }
+async function updatePackageJSONScripts(
+  tree: Tree,
+  context: string,
+  name: string
+) {
+  const projectName = `${context}-${name}`;
+  await updateJson(tree, `package.json`, (packageJson) => {
+    packageJson.scripts[
+      `serve:${projectName}:app`
+    ] = `nx serve --project ${projectName} -o`;
+    packageJson.scripts[
+      `build:${projectName}`
+    ] = `nx build --project ${projectName}`;
+    packageJson.scripts[
+      `analyze:${projectName}`
+    ] = `nx build --project ${projectName} --stats-json && webpack-bundle-analyzer dist/apps/${context}/${name}/stats.json`;
+    packageJson.scripts[
+      `lint:${projectName}`
+    ] = `nx lint --project ${projectName} && nx stylelint --project ${projectName} --fix`;
+    packageJson.scripts[
+      `test:${projectName}`
+    ] = `nx test --project ${projectName}`;
+    packageJson.scripts[
+      `e2e:${projectName}`
+    ] = `nx e2e --project ${projectName}-e2e`;
+    return packageJson;
+  });
+}
+
+async function organizeImportStatements(
+  tree: Tree,
+  context: string,
+  name: string
+) {
+  const modulePath = `apps/${context}/${name}/src/app/app.module.ts`;
+  const appModuleContent = tree.read(modulePath).toString();
+  const appModuleContentWithOrganizedImports = await organizeImports(
+    appModuleContent
+  );
+  tree.write(modulePath, appModuleContentWithOrganizedImports);
 }
